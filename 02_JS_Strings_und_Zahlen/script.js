e894172491465503ea74092c17fddb10d27b17f1
Simon <simon.metzler@student.htldornbirn.at>
2023-03-08 08:57:24 +0100

    finished 02 Strings

--- 02_JS_Strings_und_Zahlen/script.js
+++ 02_JS_Strings_und_Zahlen/script.js
@@ -4,3 +4,8 @@ let b = 3.9;
 let c = a / b;
 
 console.log("The result of " + b + " divided by 3.9 is " + c)
+
+let firstname = "Simon";
+let lastname = "Metzler";
+
+console.log("Ich heiße " + firstname + " " + lastname)
