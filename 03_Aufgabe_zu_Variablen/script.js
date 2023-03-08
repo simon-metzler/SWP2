b7066e1aa05954965495b7856071090c32a3bcf1
Simon <simon.metzler@student.htldornbirn.at>
2023-03-08 09:15:16 +0100

    finished 03 Aufgabe zu Variablen

---
+++ 03_Aufgabe_zu_Variablen/script.js
@@ -0,0 +1,20 @@
+let banana = "Banane";
+let apple = "Apple";
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let appleWeight = 0.34;
+let bananaWeight = 0.22;
+
+let result = 8 * appleWeight * applePricePerKilo;
+console.log("Preis von 8 Äpfeln " + result)
+
+let result1 = 17 * bananaWeight * bananaPricePerKilo;
+console.log("Preis von 17 Bananen " + result1)
+
+let result2 = 1000 * applePricePerKilo;
+console.log("Preis 1 Tonne Äpfel " + result2)
+
+let result3 = 1000 * bananaPricePerKilo;
+console.log("Preis von 1 Tonne Bananen " + result3)
