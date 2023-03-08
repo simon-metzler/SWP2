164d79681bb44961bda7df993bce19f181f6bfee
Simon <simon.metzler@student.htldornbirn.at>
2023-03-08 09:33:36 +0100

    half finished 04 Schleifen

---
+++ 04_Schleifen/script.js
@@ -0,0 +1,11 @@
+for(let count1 = 7; count1 < 46; count1++) {
+    console.log(count1);
+};
+
+for(let count2 = 9; count2 < 100; count2++) {
+    console.log(count2);
+};
+
+for(let count3 = 10; count3 > -1; count3--) {
+    console.log(count3);
+};
