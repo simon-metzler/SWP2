679c302887f93999cd8e72eb55f03421494ea56f
Simon <simon.metzler@student.htldornbirn.at>
2023-04-12 08:28:46 +0200

    finished 09_Aufgabe_Zeichnen

--- 09_Aufgabe_Zeichnen/script.js
+++ 09_Aufgabe_Zeichnen/script.js
@@ -7,6 +7,8 @@ for (let count1 = 0; count1 < 4; count1++) {
     }
 };
 
+console.log("")
+
 let even_numbers = 0;
 for (let count2 = 0; count2 < 101; count2++){
     if (count2 % 2 == 0){
@@ -14,3 +16,23 @@ for (let count2 = 0; count2 < 101; count2++){
     }
 }
 console.log(even_numbers)
+
+console.log("")
+
+for (let count1 = 0; count1 < 5; count1++) {
+    if (count1 == 0){
+        console.log("XXXX")
+    }
+    if (count1 == 1){
+        console.log("X   ")
+    }
+    if (count1 == 2){
+        console.log("XXXX")
+    }
+    if (count1 == 3){
+        console.log("   X")
+    }
+    if (count1 == 4){
+        console.log("XXXX")
+    }
+};
