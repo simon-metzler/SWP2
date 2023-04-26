262d50e0e0f4eb13a0ce54798f4b61e8c226590d
Simon <simon.metzler@student.htldornbirn.at>
2023-04-26 09:09:09 +0200

    finished 13_Bankomat_Beispiel

--- 13_Bankomat_Beispiel/script.js
+++ 13_Bankomat_Beispiel/script.js
@@ -9,7 +9,7 @@ const readLineAsync = () => {
   return new Promise((resolve) => {   
     readline.question("", (userRes) => {
       resolve(userRes);
-      readline.close();
+     // readline.close();
     });
   });
 };
@@ -25,30 +25,30 @@ console.log("   3. Kontostand")
 console.log("   4. Ende")
 
 while (running){
-    console.log("")
     let action = await readLineAsync();
-    console.log("")
+   
 
-    if (action == 1){
+    if (action === "1"){
         console.log("Geben Sie den Betrag ein den Sie einzahlen möchten:")
         let deposit = await readLineAsync();
-        let balance = balance + deposit;
+        balance += deposit;
         console.log("Sie haben " + deposit + " Euro eingezahlt!")
     }
-    if (action == 2){
-        console.log("Machine stopped!")
-        running = false;
+    if (action === "2"){
+        console.log("Wie viel würden Sie gerne abheben?")
+        let withdraw = await readLineAsync();
+        balance -= withdraw;
+        console.log("Sie haben " + withdraw + " Euro abgehoben.")
     }
-    if (action == 3){
-        console.log("Ihr Kontostand beträgt: " + balance)
-        
-    }
-
-    if (action == 4){
-        console.log("Machine stopped!")
-        running = false;
+    if (action === "3"){
+        console.log("Der Kontostand beträgt: " + balance)
     }
     
 
+    if (action === "4"){
+        console.log("Maschine stopt!")
+        running = false;
+    }
+    
 }
 
