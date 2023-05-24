5ad36302046a4b0631ea4cbf638934518d6fb996
Simon <simon.metzler@student.htldornbirn.at>
2023-05-24 12:13:40 +0200

    finished 01_Zugriff_JS_HTML

--- HTML & JS/01_Zugriff JS HTML/script.js
+++ HTML & JS/01_Zugriff JS HTML/script.js
@@ -1,1 +1,43 @@
-document.getElementById("content").innerHTML = "Tolle Sache"
+function plus(){
+
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseInt(num1) + parseInt(num2);
+
+    document.getElementById("result").innerHTML = "Result:" + result;
+
+}
+
+function minus(){
+
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseInt(num1) - parseInt(num2);
+
+    document.getElementById("result").innerHTML = "Result:" + result;
+
+}
+
+function multiplied(){
+
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseInt(num1) * parseInt(num2);
+
+    document.getElementById("result").innerHTML = "Result:" + result;
+
+}
+
+function divided(){
+
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseInt(num1) / parseInt(num2);
+
+    document.getElementById("result").innerHTML = "Result:" + result;
+
+}
